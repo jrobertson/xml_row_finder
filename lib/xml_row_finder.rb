983ccf07cbb4b6a7f250957eb34e997e07e7abf6
James Robertson <james@jamesrobertson.me.uk>
2022-01-24 10:28:12 +0000

    feature: The container element can now be retrieved as well as the rows.

--- lib/xml_row_finder.rb
+++ lib/xml_row_finder.rb
@@ -2,29 +2,44 @@
 
 # file: xml_row_finder.rb
 
-require 'rexle'
-require 'rexml'
-include REXML
+require 'nokorexi'
 
 
 class XMLRowFinder
 
   attr_reader :to_a
 
-  def initialize(s, debug: false)
+  def initialize(raws, debug: false)
 
     @debug = debug
-    doc = Rexle.new(s)
-    @doc2 = Document.new(s)
+
+    @doc = if raws =~ /^http/ then
+
+      nki = Nokorexi.new(url=raws) do |doc1|
+        doc1.xpath('//*[@onclick]').each do |e|
+          e.attributes['onclick'].value = ''
+        end
+
+        doc1.xpath('//*[@onmousedown]').each do |e|
+          e.attributes['onmousedown'].value = ''
+        end
+
+      end
+
+      nki.to_doc
+
+    else
+      Rexle.new(raws)
+    end
 
     a = []
 
-    doc.root.each_recursive do |e|
+    @doc.root.each_recursive do |e|
       e.attributes.delete
       a << e.backtrack.to_xpath
     end
 
-    @to_a = a2 = a.map {|e| [a.count(e), e] }
+    @to_a = a2 = a.map {|e| [a.count(e), e] }.uniq
     xpath = a2.max_by(&:first).last
 
     a3 = xpath.split('/')
@@ -35,20 +50,60 @@ class XMLRowFinder
       p1 << a3.pop; a4 << a3.join('/') + "[%s]" % p1.reverse.join('/')
     end
 
+    # using Nokogiri since Rexle has a bug with xpath predicates
+    #
+    @doc2 = Nokogiri::XML(@doc.root.xml)
+
     a5 = a4[0..-2].map do |xpath2|
-      [XPath.match(@doc2, xpath2).length, xpath2]
+      [@doc2.xpath(xpath2).length, xpath2]
     end
 
     @xpath = a5.reverse.detect {|num, xpath2| num > 1}.last
 
+    last_row = @doc2.xpath(@xpath).last
+
+    # find the container element
+    xpath = @xpath[/^[^\[]+/]
+    axpath = xpath.split('/')
+    e = @doc.element xpath
+
+    until (e.xml.include? last_row) do
+      axpath.pop
+      e = @doc.element axpath.join('/')
+    end
+
+    @cont_xpath = axpath.join('/')
+
   end
 
+  # returns the container element for all rows
+  # object returned: Rexle::Element
+  #
+  def body()
+    @doc.element @cont_xpath
+  end
+
+  # returns the xpath pointing to the container element for all rows
+  #
+  def body_xpath()
+    @cont_xpath
+  end
+
+
+  # returns rows
+  # object returned: An array of Nokogiri XML Element object
+  #
   def rows()
-    XPath.match(@doc2, @xpath)
+    @doc2.xpath @xpath
   end
 
+  # returns the xpath pointing to the rows
+  #
   def to_xpath()
     @xpath
   end
+
+  alias rows_xpath to_xpath
+
 
 end
--- xml_row_finder.gemspec
+++ xml_row_finder.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'xml_row_finder'
-  s.version = '0.2.0'
+  s.version = '0.3.0'
   s.summary = 'Attempts to find repeating rows in XHTML and returns the associated xpath.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/xml_row_finder.rb']
-  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.14')
+  s.add_runtime_dependency('nokorexi', '~> 0.5', '>=0.5.4')
   s.signing_key = '../privatekeys/xml_row_finder.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
